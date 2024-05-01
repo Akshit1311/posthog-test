8344c56e5339744defce030dfe235aa434ba167b
Akshit1311 <a4748g@gmail.com>
2024-05-01 09:26:55 +0530

    add: register event

--- src/app/_components/PHButton.tsx
+++ src/app/_components/PHButton.tsx
@@ -9,7 +9,9 @@ const PHButton = (props: Props) => {
   return (
     <button
       onClick={() => {
-        posthog.capture("my event", { property: "value" });
+        alert("Event sending to PostHog");
+        posthog.capture("Registered", { Gender: "Male", Age: 21 });
+        alert("Event sent to PostHog");
       }}
       className="bg-blue-500 px-2 py-1 rounded-md"
     >
